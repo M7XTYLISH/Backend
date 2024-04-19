10c408de1b309c6e7522037da158c2f4e3e91806
Mustafa Shahid <alyasbilal206@gmail.com>
2024-04-19 18:29:22 +0500

    add frontend domain

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import cors from "cors";
 import userRouter from "./user/userRouter.js";
 import bookRouter from "./book/bookRouter.js";
+import { config } from "./config/config.js";
 
 const app = express();
 
 // Enkable CORS for requests originating from "localhost:5513"
-app.use(cors({ origin: "localhost:5513" }));
+app.use(cors({ origin: config.frontendDomain }));
 
 // Parse incoming requests with URL-encoded payloads
 app.use(express.urlencoded({ extended: false }));
